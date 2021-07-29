4b23f92f7231c08ecd1c381844531cd2cd25a8ae
somukhannan <somukhannan8@gmail.com>
2021-07-29 14:52:28 +0000

    Refractored.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-/* eslint-disable no-console */
 import { React } from 'react';
 import './App.scss';
-import typeIt from './components/typeIt';
-import context from './core/context';
+import TypeIt from './components/typeIt';
 
 const App = () =>
 	<div className="App">
-		{ typeIt() }
-		{ console.log(context.state) }
+		{ TypeIt() }
 	</div>;
 
 export default App;
--- src/components/typeIt.js
+++ src/components/typeIt.js
@@ -1,31 +1,15 @@
-/* eslint-disable no-console */
 import { React } from 'react';
-import context from '../core/context';
+import input from './input';
+import score from './score';
+import rdmString from './randomString';
+import result from './result';
 
-const actionKeys = {
-	Enter: () => context.actions.setRndString(),
-};
+const TypeIt = () =>
+	<div>
+		{ result() }
+		{ rdmString() }
+		{ input() }
+		{ score() }
+	</div>;
 
-const typeIt = () => <div>
-	<div>{ context.state.inputString === ''
-		? <div className="note">Start Typing...</div>
-		: context.state.inputString === context.state.rndmString
-			? <div className="img_tick">
-				<span className="enter">Press Enter to Continue</span></div>
-			: <div className="img_cross"/>}
-	</div>
-	<div className="rdnStr">{context.state.rndmString}
-	</div>
-	<input
-		className="inpStr"
-		value={ context.state.inputString }
-		onChange={ (evt) =>	context.actions.setInputString(evt.target.value) }
-		// eslint-disable-next-line max-len
-		onKeyUp={ (evt) => actionKeys[evt.code] && actionKeys[evt.code]() }
-	/>
-	<div className="score">Score :
-		{ context.state.score}
-	</div>
-</div>;
-
-export default typeIt;
+export default TypeIt;
